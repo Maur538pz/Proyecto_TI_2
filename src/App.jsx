786d15c1db41bc3f1466b9bc02c2a90d8feeffe5
Maurpz <mairiciohualpa@gmail.com>
2023-12-21 09:30:41 -0500

    new 3 change path model

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { Geometry } from  './components/Geometry.jsx'
-import { Material } from './components/Material.jsx'
-import { Textures } from './components/Textures.jsx'
-import { Map } from './components/Map.jsx'
-import { Raycaster } from './components/Raycaster.jsx'
+//import { Geometry } from  './components/Geometry.jsx'
+//import { Material } from './components/Material.jsx'
+//import { Textures } from './components/Textures.jsx'
+//import { Map } from './components/Map.jsx'
+//import { Raycaster } from './components/Raycaster.jsx'
 import { Content } from './components/Content.jsx'
 import { Modelo3D } from './components/Modelo3D.jsx'
-import { Vista45 } from './components/Vista45.jsx'
+//import { Vista45 } from './components/Vista45.jsx'
 import { Name } from './components/Name.jsx'
+import { Pepo } from './components/Pepo.jsx'
 
 import {useState} from 'react'
 
@@ -29,6 +30,7 @@ function App() {
       <Modelo3D setView={setView} setSite={setSite}/>
       <Content view={view} site={site} />
       <Name  site={site}/>
+      <Pepo />
     </main>
     
  )
